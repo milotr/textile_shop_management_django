5d3c90839ed50b577d20a2119a4311653f4b119a
Long Tran <55062094+milotr@users.noreply.github.com>
2022-03-04 12:30:41 +0200

    DB migrated successfully

--- MinhPhungCompany/management/views.py
+++ MinhPhungCompany/management/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 # Create your views here.
 
 def home(request):
-    context = {}
+    customers = Customer.objects.all()
+    context = {
+        'customers' : customers,
+    }
     return render(request, 'management/index.html', context)
 
 def charts(request):
@@ -11,7 +15,13 @@ def charts(request):
     return render(request, 'management/charts.html', context)
 
 def tables(request):
-    context = {}
+    customers = Customer.objects.all()
+    products = Product.objects.all()
+    # the string in the dictionary will be used in template
+    context = {
+        'customers' : customers,
+        'products' : products,
+    } 
     return render(request, 'management/tables.html', context)
 
 def login(request):
