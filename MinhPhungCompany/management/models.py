76257928ca35d368d5e829e450a21ac7499af97e
Long Tran <55062094+milotr@users.noreply.github.com>
2022-03-07 11:48:00 +0200

    Update models.py

--- MinhPhungCompany/management/models.py
+++ MinhPhungCompany/management/models.py
@@ -2,40 +2,56 @@ from django.db import models
 
 # Create your models here.
 
+
 class Customer(models.Model):
     customer_id = models.BigAutoField(primary_key=True)
     lastname = models.CharField(max_length=64)
-    middlename = models.CharField(max_length=64, null = True)
+    middlename = models.CharField(max_length=64, null=True)
     firstname = models.CharField(max_length=64)
-    phone = models.CharField(max_length=64,null = True)
-    socialnumber = models.IntegerField(null = True)
-    customer_date = models.DateTimeField(auto_now_add = True, null = True)
+    phone = models.CharField(max_length=64, null=True)
+    socialnumber = models.IntegerField(null=True)
+    customer_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
         return f'{self.lastname} {self.middlename} {self.firstname}'
+
 
 class Product(models.Model):
     measurement_unit_choices = [
         ('m', 'meter'),
         ('y', 'yard'),
     ]
+<<<<<<< HEAD
+=======
+
+>>>>>>> a9d29420ef2428f84562d2bafb9306779a29dd0e
     product_id = models.BigAutoField(primary_key=True)
     color = models.CharField(max_length=64)
     hexcolor = models.CharField(max_length=64)
     length = models.FloatField()
     measurement_unit = models.CharField(max_length = 64, choices = measurement_unit_choices)
     type = models.CharField(max_length=64)
-    location = models.CharField(max_length=64, null = True)
-    product_date = models.DateTimeField(auto_now_add = True, null = True)
+    location = models.CharField(max_length=64, null=True)
+    length = models.FloatField()
+    measurement_unit = models.CharField(
+        max_length=64, choices=measurement_unit_choices)
+    product_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
-        return f'{self.color}-{self.hexcolor}'
+        return f'{self.product_id}-{self.color}-{self.hexcolor}'
+
 
 class Order(models.Model):
     order_id = models.BigAutoField(primary_key=True)
-    customer_id = models.ForeignKey(Customer, null=True, on_delete = models.SET_NULL)
-    product_id = models.ForeignKey(Product, null=True, on_delete = models.SET_NULL)
-    order_date = models.DateTimeField(auto_now_add=True, null = True)
+    customer_id = models.ForeignKey(
+        Customer, null=True, on_delete=models.SET_NULL)
+    product_id = models.ForeignKey(
+        Product, null=True, on_delete=models.SET_NULL)
+    order_date = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self):
+<<<<<<< HEAD
         return f'{self.order_id}-{self.customer_id}'
+=======
+        return f'{self.order_id}-{self.customer_id}-{self.product_id}'
+>>>>>>> a9d29420ef2428f84562d2bafb9306779a29dd0e
