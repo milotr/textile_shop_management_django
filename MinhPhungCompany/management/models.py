23b5c803cd1368f263c88c27a6c36b063b264eb3
Long Tran <55062094+milotr@users.noreply.github.com>
2023-10-12 18:02:49 +0700

    Rewrite and implement the database

--- MinhPhungCompany/management/models.py
+++ MinhPhungCompany/management/models.py
@@ -3,7 +3,8 @@ from django.db import models
 class Customer(models.Model):
     customer_id = models.BigAutoField(primary_key=True)
     name = models.CharField(max_length=64)
-    phone = models.CharField(max_length=64, null=True)
+    phone = models.CharField(max_length=64)
+    address = models.CharField(max_length=64)
     socialnumber = models.IntegerField(null=True)
     customer_date = models.DateTimeField(auto_now_add=True, null=True)
 
@@ -36,7 +37,7 @@ class Color(models.Model):
     measurement_unit = models.CharField(max_length = 64, choices = measurement_unit_choices)
     location = models.CharField(max_length=64, null=True)
     product_date = models.DateTimeField(auto_now_add=True, null=True)
-
+    sold = models.BooleanField()
     def __str__(self):
         return f'{self.length}{self.measurement_unit}'
 
@@ -46,6 +47,18 @@ class Order(models.Model):
     customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Color, null=True, on_delete=models.SET_NULL)
     order_date = models.DateTimeField(auto_now_add=True, null=True)
+    delivery_choices = [
+        ('Đã giao', 'Đã giao'),
+        ('Chưa giao', 'Chưa giao'),
+        ('Đang giao', 'Đang giao').
+    ]
+    delivery = models.CharField(max_length=64, choices=delivery_choices)
+    deliverer = models.CharField(max_length=64)
+    payment_choices = [
+        ('Chưa thanh toán', 'Chưa thanh toán'),
+        ('Đã thanh toán', 'Đã thanh toán'),
+    ]
+    payment = models.CharField(max_length=64, choices=payment_choices)
 
     def __str__(self):
         return f'{self.customer}-{self.product}'
